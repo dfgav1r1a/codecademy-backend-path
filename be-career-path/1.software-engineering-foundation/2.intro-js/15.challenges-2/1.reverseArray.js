af6b788e2280707c90ef6723eb6e02f1ea6fcc75
dfgaviria <dfgaviria.dev@gmail.com>
2023-05-12 16:20:22 -0400

    adding some challenges for JS II

---
+++ be-career-path/1.software-engineering-foundation/2.intro-js/15.challenges-2/1.reverseArray.js
@@ -0,0 +1,20 @@
+// Write your code here:
+
+function reverseArray(array) {
+    newArray = [];    
+    for (let i = array.length - 1; i >= 0; i--) {
+        newArray.push(array[i]);
+    }
+    return newArray
+}
+
+
+// When you're ready to test your code, uncomment the below and run:
+ 
+const sentence = ['sense.','make', 'all', 'will', 'This'];
+
+console.log(reverseArray(sentence)) 
+// Should print ['This', 'will', 'all', 'make', 'sense.'];
+
+
+
---
+++ be-career-path/1.software-engineering-foundation/2.intro-js/15.challenges-2/6.greeting.js
@@ -0,0 +1,25 @@
+// Write a function shoutGreetings() that takes in an array of strings and returns a new array. This new array should contain all the strings from the argument array but with capitalized letters and an exclamation point appended to the end: 'heya' will become 'HEYA!'
+
+// You can use any technique you want to accomplish this task.
+
+// You can test your function when you’re ready by passing in the greetings array or by making your own array!
+
+// Write your code here:
+
+function shoutGreetings(array) {
+    const newArray = array.map(string => {
+        return string.toUpperCase().concat('!');
+    })
+    return newArray
+}
+
+
+
+
+
+
+// Feel free to uncomment out the code below to test your function!
+const greetings = ['hello', 'hi', 'heya', 'oi', 'hey', 'yo'];
+
+console.log(shoutGreetings(greetings))
+// Should print [ 'HELLO!', 'HI!', 'HEYA!', 'OI!', 'HEY!', 'YO!' ]
