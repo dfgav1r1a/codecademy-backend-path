f5a079fd813f89a377a4e3ad42101d4838a831d5
dfgaviria <dfgaviria.dev@gmail.com>
2023-12-04 17:56:15 -0500

    added GET minion routes

--- be-career-path/2.backend-development/7.express-js/7.project-boss-machine/server/api.js
+++ be-career-path/2.backend-development/7.express-js/7.project-boss-machine/server/api.js
@@ -1,6 +1,23 @@
 const express = require('express');
 const apiRouter = express.Router();
+const db = require('./db');
 
+apiRouter.get('/minions', (req, res, next) => {
+    const minions = db.getAllFromDatabase('minions');
+    res.status(200).send(minions) 
+});
 
+apiRouter.get('/minions/:minionId', (req, res, next) => {
+    const id = parseInt(req.params.minionId);
+    const minion = db.getFromDatabaseById('minions', id);
+    res.status(200).send(minion);
+});
+
+apiRouter.post('/minions', (req, res, next) => {
+    const newMinion = req.body;
+    console.log(newMinion);
+    const createdMinion = db.addToDatabase('minions', newMinion)
+    res.status(201).send(createdMinion)
+});
 
 module.exports = apiRouter;
