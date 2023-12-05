7089598180b14458451e95fdb474c4b69a16814c
dfgaviria <dfgaviria.dev@gmail.com>
2023-12-05 17:37:05 -0500

    added routes for ideas, created api test for ideas route

--- be-career-path/2.backend-development/7.express-js/7.project-boss-machine/server/api.js
+++ be-career-path/2.backend-development/7.express-js/7.project-boss-machine/server/api.js
@@ -2,6 +2,7 @@ const express = require('express');
 const apiRouter = express.Router();
 const db = require('./db');
 
+//Minions API
 apiRouter.get('/minions', (req, res, next) => {
     const minions = db.getAllFromDatabase('minions');
     res.status(200).send(minions) 
@@ -11,7 +12,8 @@ apiRouter.get('/minions/:minionId', (req, res, next) => {
     const id = req.params.minionId;
     const minion = db.getFromDatabaseById('minions', id);
     console.log(minion);
-    res.status(200).send(minion);
+    minion ? res.status(200).send(minion)
+           : res.status(404).send('Minion does not exist')
 });
 
 apiRouter.post('/minions', (req, res, next) => {
@@ -39,7 +41,47 @@ apiRouter.put('/minions/:minionId', (req, res, next) => {
 apiRouter.delete('/minions/:minionId', (req, res, next) => {
     const id = req.params.minionId;
     const toDelete = db.deleteFromDatabasebyId('minions', id);
-    !toDelete ? res.status(400).send('There is no minion to kill!') : res.status(200).send('Minion was eliminated and nobody noticed')
+    !toDelete ? res.status(400).send('There is no minion to kill!') 
+              : res.status(201).send('Minion was eliminated and nobody noticed')
 })
+
+//ideas API
+apiRouter.get('/ideas', (req, res, next) => {
+    const allIdeas = db.getAllFromDatabase('ideas');
+    res.status(200).send(allIdeas);
+});
+
+apiRouter.get('/ideas/:ideaId', (req, res, next) => {
+    const id = req.params.ideaId;
+    const idea = db.getFromDatabaseById('ideas', id);
+    idea ? res.status(200).send(idea)
+         : res.status(400).send('Idea does not exist!');
+});
+
+apiRouter.post('/ideas', (req, res, next) => {
+    const newIdea = db.addToDatabase('ideas', req.body);
+    !newIdea ? res.status(400).send('Something went wrong, please try again')
+             : res.status(201).send(newIdea);
+});
+
+apiRouter.put('/ideas/:ideaId', (req, res, next) => {
+    const id = req.params.ideaId;
+    const {name, description, weeklyRevenue, numWeeks} = req.body;
+    const updatedIdeaObj = {
+        id: id,
+        name: name,
+        description: description,
+        weeklyRevenue: weeklyRevenue,
+        numWeeks: numWeeks
+    };
+    const updatedIdea = db.updateInstanceInDatabase('ideas', updatedIdeaObj);
+    !updatedIdea ? res.status(400).send('Uh Oh something went wrong, please try again')
+                : res.status(200).send(updatedIdea)
+    
+});
+
+apiRouter.delete('/ideas/:ideaId', (req, res, next) => {
+ const id = req.params.ideaId;
+});
 
 module.exports = apiRouter;
