06729ecd948def95bdab5051f335e9aff4f11aca
dfgaviria <dfgaviria.dev@gmail.com>
2023-12-11 16:46:46 -0500

    finished boss machine project

--- be-career-path/2.backend-development/7.express-js/7.project-boss-machine/server/ideasAPI.js
+++ be-career-path/2.backend-development/7.express-js/7.project-boss-machine/server/ideasAPI.js
@@ -3,7 +3,15 @@ const checkMillionDollarIdea = require('./checkMillionDollarIdea');
 const ideasRouter = express.Router();
 const db = require('./db');
 
-// ideasRouter.use(checkMillionDollarIdea);
+ideasRouter.param('ideaId', (req, res, next, id) => {
+    const idea = db.getFromDatabaseById('ideas', id);
+    if (!idea) {
+        res.status(404).send('Idea does not exist!');
+    } else {
+        req.idea = idea;
+        next();
+    }
+});
 
 ideasRouter.get('/', (req, res, next) => {
     const allIdeas = db.getAllFromDatabase('ideas');
@@ -11,27 +19,19 @@ ideasRouter.get('/', (req, res, next) => {
 });
 
 ideasRouter.get('/:ideaId', (req, res, next) => {
-    const id = req.params.ideaId;
-    const idea = db.getFromDatabaseById('ideas', id);
-    idea ? res.status(200).send(idea)
-        : res.status(404).send('Idea does not exist!');
+    res.status(200).send(req.idea);
 });
 
 ideasRouter.post('/', checkMillionDollarIdea, (req, res, next) => {
     const newIdea = db.addToDatabase('ideas', req.body);
     !newIdea ? res.status(400).send('Something went wrong, please try again')
-        : res.status(201).send(newIdea);
+             : res.status(201).send(newIdea);
 });
 
 ideasRouter.put('/:ideaId', checkMillionDollarIdea, (req, res, next) => {
-    const id = req.params.ideaId;
-    const isFound = db.getFromDatabaseById('ideas', id);
-    if (!isFound) {
-        res.status(404).send('Idea does not exist');
-    } else {
         const { name, description, weeklyRevenue, numWeeks } = req.body;
         const updatedIdeaObj = {
-            id: id,
+            id: req.idea.id,
             name: name,
             description: description,
             weeklyRevenue: weeklyRevenue,
@@ -39,12 +39,10 @@ ideasRouter.put('/:ideaId', checkMillionDollarIdea, (req, res, next) => {
         };
         const updatedIdea = db.updateInstanceInDatabase('ideas', updatedIdeaObj);
         res.status(200).send(updatedIdea)
-    }
 });
 
 ideasRouter.delete('/:ideaId', (req, res, next) => {
-    const id = req.params.ideaId;
-    const deletedIdea = db.deleteFromDatabasebyId('ideas', id);
+    const deletedIdea = db.deleteFromDatabasebyId('ideas', req.idea.id);
     !deletedIdea ? res.status(404).send('Seems like this idea never existed')
                  : res.status(204).send('Finally you got rid of this idea.')
 });
--- be-career-path/2.backend-development/7.express-js/7.project-boss-machine/server/minionsAPI.js
+++ be-career-path/2.backend-development/7.express-js/7.project-boss-machine/server/minionsAPI.js
@@ -2,6 +2,15 @@ const express = require('express');
 const minionsRouter = express.Router();
 const db = require('./db');
 
+minionsRouter.param('minionId', (req, res, next, id) => {
+    const minion = db.getFromDatabaseById('minions', id);
+    if (!minion) {
+        res.status(404).send("minion does not exist")
+    } else {
+        req.minion = minion;
+        next();
+    }
+});
 
 minionsRouter.get('/', (req, res, next) => {
     const minions = db.getAllFromDatabase('minions');
@@ -9,10 +18,7 @@ minionsRouter.get('/', (req, res, next) => {
 });
 
 minionsRouter.get('/:minionId', (req, res, next) => {
-    const id = req.params.minionId;
-    const minion = db.getFromDatabaseById('minions', id);
-    minion ? res.status(200).send(minion)
-           : res.status(404).send('Minion does not exist')
+    res.send(req.minion);
 });
 
 minionsRouter.post('/', (req, res, next) => {
@@ -22,31 +28,68 @@ minionsRouter.post('/', (req, res, next) => {
 });
 
 minionsRouter.put('/:minionId', (req, res, next) => {
-    const id = req.params.minionId;
-    const isFound = db.getFromDatabaseById('minions', id);
-    if (!isFound) {
-        res.status(404).send('Minion does not exist');
-    } else {
-        //if it does then the minion will be updated
-        const { title, name, weaknesses, salary } = req.body;
-        const updatedMinionObj = {
-            id: id,
-            name: name,
-            title: title,
-            weaknesses: weaknesses,
-            salary: salary
-        }
-        const updatedMinion = db.updateInstanceInDatabase('minions', updatedMinionObj);
-        console.log(updatedMinion);
-        res.status(200).send(updatedMinion);
+    const { title, name, weaknesses, salary } = req.body;
+    const updatedMinionObj = {
+        id: req.minion.id,
+        name: name,
+        title: title,
+        weaknesses: weaknesses,
+        salary: salary
     }
+    const updatedMinion = db.updateInstanceInDatabase('minions', updatedMinionObj);
+    res.status(200).send(updatedMinion);
 });
 
 minionsRouter.delete('/:minionId', (req, res, next) => {
-    const id = req.params.minionId;
-    const toDelete = db.deleteFromDatabasebyId('minions', id);
+    const toDelete = db.deleteFromDatabasebyId('minions', req.minion.id);
     !toDelete ? res.status(404).send('There is no minion to kill!')
-        : res.status(204).send('Minion was eliminated and nobody noticed')
+              : res.status(204).send('Minion was eliminated and nobody noticed')
+});
+
+
+//work API
+
+minionsRouter.get('/:minionId/work', (req, res, next) => {
+    const allWork = db.getAllFromDatabase('work');
+    const minionDuties = allWork.filter(work => work.minionId === req.minion.id);
+    res.status(200).send(minionDuties);
+});
+
+minionsRouter.post('/:minionId/work', (req, res, next) => {
+    const toAdd = req.body;
+    toAdd.minionId = req.params.minionId;
+    const newWork = db.addToDatabase('work', toAdd);
+    res.status(201).send(newWork);
+});
+
+//adding middleware for the workId parameter
+
+minionsRouter.param('workId', (req, res, next, id) => {
+    const work = db.getFromDatabaseById('work', id);
+    if (!work) {
+        res.status(404).send('Work not found');
+    } else {
+        req.work = work;
+        next();
+    }
+})
+
+minionsRouter.put('/:minionId/work/:workId', (req, res, next) => {
+    if (req.params.minionId !== req.body.minionId) {
+        res.status(400).send();
+    } else {
+        const toDb = db.updateInstanceInDatabase('work', req.body);
+        res.status(201).send(toDb);
+    }
+});
+
+minionsRouter.delete('/:minionId/work/:workId', (req, res, next) => {
+    const toDelete = db.deleteFromDatabasebyId('work', req.params.workId);
+    if (toDelete) {
+        res.status(204).send();
+    } else {
+        res.status(404).send('There is nothing to delete');
+    }
 });
 
 module.exports = minionsRouter;
